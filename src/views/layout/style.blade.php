fab86f83411e329e33a8f8e761c167cf99e84aaa
Shino303 <locshino123@gmail.com>
2024-06-10 13:15:41 +0700

    Update style.blade.php

--- src/views/layout/style.blade.php
+++ src/views/layout/style.blade.php
@@ -1,13 +1,15 @@
-<link rel="stylesheet" href="./../public/vendors/feather/feather.css">
-<link rel="stylesheet" href="./../public/vendors/ti-icons/css/themify-icons.css">
-<link rel="stylesheet" href="./../public/vendors/css/vendor.bundle.base.css">
-<!-- endinject -->
-<!-- Plugin css for this page -->
-<link rel="stylesheet" href="./../public/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-<link rel="stylesheet" href="./../public/vendors/ti-icons/css/themify-icons.css">
-<link rel="stylesheet" type="text/css" href="./../public/js/select.dataTables.min.css">
-<!-- End plugin css for this page -->
-<!-- inject:css -->
-<link rel="stylesheet" href="./../public/css/vertical-layout-light/style.css">
-<!-- endinject -->
-<link rel="shortcut icon" href="./../public/images/favicon.png" />
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{ BASE_URL . 'public/vendors/feather/feather.css' }}">
+  <link rel="stylesheet" href="{{ BASE_URL . 'public/vendors/ti-icons/css/themify-icons.css' }}">
+  <!-- endinject -->
+
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ BASE_URL . 'public/vendors/css/vendor.bundle.base.css' }}">
+  <link rel="stylesheet" href="{{ BASE_URL . 'public/vendors/datatables.net-bs4/dataTables.bootstrap4.css' }}">
+  <link rel="stylesheet" href="{{ BASE_URL . 'public/vendors/ti-icons/css/themify-icons.css' }}">
+  <!-- End plugin css for this page -->
+  <link rel="stylesheet" type="public/text/css" href="{{ BASE_URL . 'js/select.dataTables.min.css' }}">
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ BASE_URL . 'public/css/vertical-layout-light/style.css' }}">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{ BASE_URL . 'public/images/favicon.png' }}">

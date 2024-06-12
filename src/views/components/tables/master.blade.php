b3ee34f0a8b3ddcf6f2d75c17585bd7e6688c334
Shino303 <locshino123@gmail.com>
2024-06-12 16:32:08 +0700

    Update navbar, sidebar

---
+++ src/views/components/tables/master.blade.php
@@ -0,0 +1,58 @@
+<div class="col-lg-12 stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Table with contextual classes</h4>
+        <p class="card-description">Add class <code>.table-{color}</code></p>
+        <div class="table-responsive pt-3">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First name</th>
+                <th>Product</th>
+                <th>Amount</th>
+                <th>Deadline</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="table-info">
+                <td>1</td>
+                <td>Herman Beck</td>
+                <td>Photoshop</td>
+                <td>$ 77.99</td>
+                <td>May 15, 2015</td>
+              </tr>
+              <tr class="table-warning">
+                <td>2</td>
+                <td>Messsy Adam</td>
+                <td>Flash</td>
+                <td>$245.30</td>
+                <td>July 1, 2015</td>
+              </tr>
+              <tr class="table-danger">
+                <td>3</td>
+                <td>John Richards</td>
+                <td>Premeire</td>
+                <td>$138.00</td>
+                <td>Apr 12, 2015</td>
+              </tr>
+              <tr class="table-success">
+                <td>4</td>
+                <td>Peter Meggik</td>
+                <td>After effects</td>
+                <td>$ 77.99</td>
+                <td>May 15, 2015</td>
+              </tr>
+              <tr class="table-primary">
+                <td>5</td>
+                <td>Edward</td>
+                <td>Illustrator</td>
+                <td>$ 160.25</td>
+                <td>May 03, 2015</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>

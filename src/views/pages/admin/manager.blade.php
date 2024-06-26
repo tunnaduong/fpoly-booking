ecdb066f67297c00e269aa4953ae8269b0c255ad
quynhtranhuu <quynhthpn00114@gmail.com>
2024-06-26 16:16:32 +0700

    update

---
+++ src/views/pages/admin/manager.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+@section('content')
+    {{-- Title start --}}
+    <div class="row">
+        @include('components.mainTitleContent.master');
+    </div>
+    {{-- Title end  --}}
+    <div class="row">
+        <button type="submit"><a href="">Add</a></button>
+        <input type="search" placeholder="Search" name="txtsearch" id="">
+        <button type="submit"><a href="">Search</a></button>
+    </div>
+    <div class="row">
+        @include('components.tables.master');
+    </div>
+@endsection
